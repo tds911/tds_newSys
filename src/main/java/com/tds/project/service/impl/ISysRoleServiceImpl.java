dc625aaaea37b384f09cc6a916effeec55cf3dcc
123 <794644507@qq.com>
2022-07-25 14:14:06 +0800

    管理员权限

--- src/main/java/com/tds/project/service/impl/ISysRoleServiceImpl.java
+++ src/main/java/com/tds/project/service/impl/ISysRoleServiceImpl.java
@@ -1,15 +1,25 @@
 package com.tds.project.service.impl;
 
 
+import com.tds.project.mapper.SysRoleMenuMapper;
 import com.tds.project.service.ISysRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class ISysRoleServiceImpl implements ISysRoleService {
+    @Autowired
+    private SysRoleMenuMapper roleMenuMapper;
+
     @Override
     public Set<String> selectRolePermissionByUserId(Long userId) {
-        return null;
+        Set<String> perms=new HashSet<String>();
+        List<String> stringList = roleMenuMapper.selectMenuPermsByUserId(userId);
+        perms.addAll(stringList);
+        return perms;
     }
 }
